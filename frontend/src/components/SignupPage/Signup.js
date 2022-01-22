20b7f139842f9e1b91a572c04257a777d39a0b24
VincentPheng <vpheng@calpoly.edu>
2022-01-21 18:02:39 -0800

    Create react states for signup fields

--- frontend/src/components/SignupPage/Signup.js
+++ frontend/src/components/SignupPage/Signup.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
-import { Box, Button, Heading, Stack, Input, Checkbox} from "@chakra-ui/react"
+import { Box, Button, Heading, Stack, Input, Checkbox } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <Box>
       <Heading mb={5}>Sign Up</Heading>
       <Stack>
-        <Input placeholder='Name'/>
-        <Input placeholder='Email'/>
-        <Input placeholder='Phone Number'/>
-        <Input placeholder='Password' type={showPassword ? 'text' : 'password'}/>
-        <Checkbox onChange={() => setShowPassword(!showPassword)}>Show password</Checkbox>
+        <Input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <Input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <Input
+          placeholder="Phone Number"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type={showPassword ? "text" : "password"}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Checkbox onChange={() => setShowPassword(!showPassword)}>
+          Show password
+        </Checkbox>
       </Stack>
       <Link to="/">
         <Button>Go back to home</Button>
