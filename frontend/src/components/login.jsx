6b83d5b5c4ef6a6d2c92f08e2196766d66a517ac
thebarrettlo <beebearlo@gmail.com>
2022-02-02 20:46:14 -0800

    Inline-styled Log In page with Chakra UI

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,55 +1,99 @@
 import React, { useState } from 'react';
-import { Container, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  VStack,
+  Flex,
+  Image,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button
+} from '@chakra-ui/react';
+import {
+  ViewIcon,
+  ViewOffIcon
+} from '@chakra-ui/icons';
 
-import logo from './assets/images/RISE-logo-full-1.png';
+import logo from '../assets/images/RISE-logo-full-1.png';
 
+{/* !!! Add react-router to forgot password and create new account !!! */}
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => setShow(!show);
 
   let submitLogin = (email, password) => {
-    console.log("submitted email: " + email);
-    console.log("submitted password: " + password);
+    console.log('submitted email: ' + email);
+    console.log('submitted password: ' + password);
+  }
+
+  const mainStyle = {
+    backgroundColor: '#F6F6F6'
   }
 
   return (
-      <main>
-        <Container>
-          <VStack>
-            <image id="logo" src={logo} />
-            <h1>Volunteer System</h1>
-            <form id="loginForm">
+      <main style={ mainStyle }>
+        <Container h='100vh'>
+          <VStack justify='center' h='100%'>
+            <Image id='logo' src={logo} alt='RISE logo in color.' mixBlendMode='multiply' mb='1rem'/>
+            <Heading as='h1' size='3xl' align='center' color='#024E6B'>Volunteer System</Heading>
+            <FormControl id='loginForm' mt='1rem' pt='2rem'>
 
-              <label htmlFor="emailField">Email</label>
-              <input
-                type="email"
-                id="emailField"
-                placeholder=""
+              <FormLabel htmlFor='emailField'>Email</FormLabel>
+              <Input
+                type='email'
+                id='emailField'
+                placeholder=''
                 value={ email }
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
-                required
+                isRequired
+                variant='filled'
+                background='white'
+                mb='1rem'
               />
 
-              <label htmlFor="passwordField">Password</label>
-              <input
-                type="password"
-                id="passwordField"
-                placeholder=""
-                value={ password }
-                onChange={ (e) => {
-                  setPassword(e.target.value);
-                }}
-                required
-              />
-              <label htmlFor="passwordField" id="forgotPassword">Forgot your password?</label>
+              <FormLabel htmlFor='passwordField'>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={show ? 'text' : 'password'}
+                  id='passwordField'
+                  placeholder=''
+                  value={ password }
+                  onChange={ (e) => {
+                    setPassword(e.target.value);
+                  }}
+                  isRequired
+                  variant='filled'
+                  background='white'
+                />
+                <InputRightElement width='auto' right='0.5rem'>
+                  <Button h='1.75rem' size='sm' leftIcon={show ? <ViewOffIcon /> : <ViewIcon />} onClick={ handleClick }>
+                    {show ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              
+              <FormLabel htmlFor='passwordField' id='forgotPassword' float='right'>Forgot your password?</FormLabel>
 
-              <button type="button" onClick={ () => submitLogin(email, password) }>
+              <Button
+                isFullWidth
+                my='1rem'
+                onClick={ () => submitLogin(email, password) }
+                background='#024E6B'
+                color='white'
+              >
                 Log In
-              </button>
-              <span id="createAccount">First time? <a href="#" title="Create a new Lumina volunteer account">Create a new account</a></span>
-            </form>
+              </Button>
+              <Box id='createAccount' textAlign='center'>First time? <a href='#' title='Create a new Lumina volunteer account'>Create a new account</a></Box>
+            </FormControl>
           </VStack>
         </Container>
       </main>
