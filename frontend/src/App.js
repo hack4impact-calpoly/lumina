ecb5c4bb1c2d445b8501ed42fa6f69addf6094d4
thebarrettlo <beebearlo@gmail.com>
2022-02-08 15:28:30 -0800

    Added react-router-dom@6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import * as React from 'react';
-
-import { ChakraProvider } from '@chakra-ui/react';
-import Login from './components/login';
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/LoginPage/Login";
+import Signup from "./components/SignupPage/Signup";
+import HomePage from "./components/HomePage/HomePage";
+import theme from "./theme";
 
 function App() {
   return (
-    <ChakraProvider>
-      <div className="App">
-        <Login />
-      </div>
+    <ChakraProvider theme={theme}>
+      <Box
+        mt={5}
+        mb={5}
+      >
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/sign-up" element={<Signup />} />
+          </Routes>
+        </Router>
+      </Box>
     </ChakraProvider>
   );
 }
